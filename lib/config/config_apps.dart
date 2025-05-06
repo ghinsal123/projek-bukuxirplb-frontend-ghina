a22015a1e338d06f36af5fb881fb7437c8fc428f
ghinsal123 <gsalsabila570@gmail.com>
2025-05-06 13:29:08 +0700

    menampilkan halaman login dan daftar buku

--- lib/config/config_apps.dart
+++ lib/config/config_apps.dart
@@ -1,15 +1,17 @@
-class ConfigApps { //Membuat kelas ConfigApps, berfungsi sebagai tempat menyimpan konfigurasi aplikasi seperti URL dan endpoint API.
-  final String url = 'http://10.0.2.2:8000'; //url utama ke server lokal. 10.0.2.2 adalah IP khusus yang digunakan oleh Android Emulator untuk mengakses localhost komputer host.
+
+class ConfigApps {
+  static String url = 'http://10.0.2.2:8000';
+  // static String url = 'http://localhost:8000';
   //Login backend
-  final String login = '/auth/login'; //Endpoint untuk melakukan proses login.
-  final String logout = '/auth/logout'; //Endpoint untuk logout.
-  final String check = '/auth/checkstatus'; //Endpoint untuk mengecek status login/token user, apakah masih valid atau tidak.
+  static String login = '/api/auth/login';
+  static String logout = '/api/auth/logout';
+  static String check = '/api/auth/checkstatus';
 
   //data buku
-  final String listbuku = '/buku'; //Endpoint untuk mendapatkan semua data buku (GET request).
-  final String detilbuku = '/buku/detil/'; //Endpoint untuk melihat detail satu buku, biasanya ditambah ID di belakang. Contoh: /buku/detil/5.
-  final String caribuku = '/buku/cari'; //Endpoint untuk mencari buku, kemungkinan berdasarkan keyword (POST request).
-  final String tambahbuku = '/buku/tambah'; //Endpoint untuk menambahkan data buku baru (POST request).
-  final String hapusbuku = '/buku/hapus/'; //Endpoint untuk menghapus buku. Biasanya ditambah ID buku di belakang. Contoh: /buku/hapus/3.
-  final String editbuku = '/buku/edit/'; //Endpoint untuk mengedit atau update data buku. Juga ditambah ID di belakang. Contoh: /buku/edit/4.
+  static String listbuku = '/api/buku';
+  static String detilbuku = '/api/buku/detil/';
+  static String caribuku = '/api/buku/cari';
+  static String tambahbuku = '/api/buku/tambah';
+  static String hapusbuku = '/api/buku/hapus/';
+  static String editbuku = '/api/buku/edit/';
 }
--- lib/pages/login/login_pages.dart
+++ lib/pages/login/login_pages.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'package:bukuxirplb/config/config_apps.dart';
+import 'package:bukuxirplb/provider/token_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
 
 class LoginPages extends StatefulWidget {
   const LoginPages({super.key});
@@ -10,160 +16,217 @@ class LoginPages extends StatefulWidget {
 class _LoginPagesState extends State<LoginPages> {
   final TextEditingController emailTextController = TextEditingController();
   final TextEditingController passwordTextController = TextEditingController();
-  final _formKey = GlobalKey<FormState>();
+  final passwordVisible = false;
+  var isLoading = false;
+  final _formkey = GlobalKey<FormState>();
+
+  //Future<bool> login() async {
+  Future<void> login() async {
+    isLoading = true;
+    //Variabel URL Backend pada config_apps.dart
+    final dynamic apiUrl = Uri.parse(ConfigApps.url + ConfigApps.login);
+    //Mengambil nilai dari email dan password
+    Map<String, String> postData = {
+      "email": emailTextController.text,
+      "password": passwordTextController.text
+    };
+    //try {
+    final response = await http.post(apiUrl,
+        headers: {"Content-Type": "application/x-www-form-urlencoded"},
+        body: postData);
+    Map<String, dynamic> responseData = jsonDecode(response.body);
+
+    if (response.statusCode == 200 && responseData['status']) {
+      Provider.of<TokenProvider>(context, listen: false)
+          .setCurrentToken(responseData['token']);
+      //TokenProvider().setCurrentToken(responseData["token"]);
+      log(context.read<TokenProvider>().getCurrentToken.toString());
+      //log(Provider.of<TokenProvider>(context).getCurrentToken as String);
+      //Provider.of<TokenProvider>(context, listen: false).setCurrentToken(responseData['token']);
+      //return true;
+    } else {
+      //return false;
+      log("Login Gagal");
+    }
+    // } catch (error) {
+    //return false;
+    /// log("login gagal 2");
+    //log(responseData['']);
+    //log(apiUrl.toString());
+    //log(error.toString());
+    //}
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: true,
-      backgroundColor: const Color.fromARGB(217, 125, 28, 73),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              const Center(
-                heightFactor: 1.2,
-                widthFactor: 1.2,
-                child: Padding(
-                  padding: EdgeInsets.fromLTRB(40, 90, 40, 30),
-                  child: Text(
-                    "MY BOOK",
-                    style: TextStyle(
-                      color: Color.fromARGB(200, 255, 255, 255),
+      extendBody: true,
+      extendBodyBehindAppBar: true,
+      resizeToAvoidBottomInset: false,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const Center(
+              heightFactor: 1.4,
+              widthFactor: 5.0,
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(40, 70, 40, 10),
+                child: Text(
+                  "BukuKu",
+                  style: TextStyle(
+                      color: Color.fromARGB(201, 8, 160, 112),
                       fontSize: 38,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
+                      fontWeight: FontWeight.bold),
                 ),
               ),
-              const Center(
-                heightFactor: 1.2,
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text(
-                        "Login",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w500,
-                          fontSize: 18,
-                          color: Colors.white,
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              Form(
-                key: _formKey,
-                child: Container(
-                  margin: const EdgeInsets.symmetric(vertical: 10),
-                  width: MediaQuery.of(context).size.width / 1.2,
-                  decoration: BoxDecoration(
-                    color: Colors.white.withOpacity(0.95),
-                    borderRadius: const BorderRadius.all(Radius.circular(20)),
+            ),
+            const Center(
+              heightFactor: 0.9,
+              child: Column(children: [
+                Padding(
+                  padding: EdgeInsets.all(9),
+                  child: Text(
+                    "Login",
+                    style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 18,
+                        color: Color.fromARGB(246, 75, 75, 75)),
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(30, 20, 30, 5),
-                    child: Column(
-                      children: [
-                        const Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Text(
-                            "Email",
-                            style: TextStyle(
-                              fontWeight: FontWeight.w500,
-                              fontSize: 18,
-                              color: Colors.black,
-                            ),
-                            textAlign: TextAlign.left,
+                )
+              ]),
+            ),
+            Opacity(
+                opacity: 1,
+                child: Form(
+                    key: _formkey,
+                    child: Container(
+                        width: MediaQuery.of(context).size.width / 1.2,
+                        height: MediaQuery.of(context).size.height / 2.5,
+                        decoration: const BoxDecoration(
+                            color: Color.fromARGB(0, 223, 24, 24),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(20))),
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(2, 10, 2, 5),
+                          child: Column(
+                            children: [
+                              //Username Field
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(0, 2, 0, 2),
+                                child: TextFormField(
+                                  keyboardType: TextInputType.emailAddress,
+                                  controller: emailTextController,
+                                  autofocus: true,
+                                  validator: (value) {
+                                    if (value == null || value.isEmpty) {
+                                      return "Email tidak boleh kosong";
+                                    }
+                                    return null;
+                                  },
+                                  textAlign: TextAlign.start,
+                                  maxLines: 1,
+                                  decoration: InputDecoration(
+                                      fillColor:
+                                          const Color.fromARGB(0, 223, 24, 24),
+                                      filled: true,
+                                      hintText: "Email/ Username",
+                                      prefixIcon: const Icon(
+                                        Icons.email,
+                                        color: Colors.black,
+                                      ),
+                                      focusColor: Colors.black,
+                                      focusedErrorBorder: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(20)),
+                                      errorBorder: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(20)),
+                                      focusedBorder: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(20)),
+                                      enabledBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(20),
+                                      )),
+                                ),
+                              ),
+                              //Password Field
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                                child: TextFormField(
+                                  obscureText: true,
+                                  controller: passwordTextController,
+                                  textAlign: TextAlign.start,
+                                  maxLines: 1,
+                                  validator: (value) {
+                                    if (value == null || value.isEmpty) {
+                                      return "Password tidak boleh kosong";
+                                    }
+                                    return null;
+                                  },
+                                  decoration: InputDecoration(
+                                      hintText: "password",
+                                      suffixIcon: const Icon(Icons.visibility),
+                                      prefixIcon: const Icon(
+                                        Icons.key,
+                                        color: Colors.black,
+                                      ),
+                                      focusedErrorBorder: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(20)),
+                                      errorBorder: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(20)),
+                                      focusedBorder: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(20)),
+                                      enabledBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(20),
+                                      )),
+                                ),
+                              ),
+                              const Padding(
+                                padding: EdgeInsets.fromLTRB(0, 5, 10, 9),
+                                child: Align(
+                                  alignment: Alignment.centerRight,
+                                  child: Text(
+                                    "Lupa Password? klik disini",
+                                    textAlign: TextAlign.end,
+                                    style: TextStyle(
+                                        fontSize: 14,
+                                        color:
+                                            Color.fromARGB(201, 8, 160, 112)),
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(0, 5, 0, 9),
+                                child: MaterialButton(
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10)),
+                                  minWidth: MediaQuery.of(context).size.width,
+                                  color: const Color.fromARGB(201, 8, 160, 112),
+                                  onPressed: () {
+                                    //Validasi form input
+                                    if (_formkey.currentState!.validate()) {
+                                      //aksi login
+                                      login();
+                                    }
+                                  },
+                                  child: const Text(
+                                    "Login",
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ),
+                              )
+                              /*
+                              const Text(
+                                "Lupa Password?, klik disini",
+                                style: TextStyle(color: Color.fromARGB(255, 15, 2, 109)),)
+                                */
+                            ],
                           ),
-                        ),
-                        TextFormField(
-                          controller: emailTextController,
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return "Email tidak boleh kosong";
-                            }
-                            return null;
-                          },
-                          decoration: InputDecoration(
-                            hintText: "Email/Username",
-                            prefixIcon: const Icon(
-                              Icons.email,
-                              color: Colors.black,
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 20),
-                        const Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Text(
-                            "Password",
-                            style: TextStyle(
-                              fontWeight: FontWeight.w500,
-                              fontSize: 18,
-                              color: Colors.black,
-                            ),
-                            textAlign: TextAlign.left,
-                          ),
-                        ),
-                        TextFormField(
-                          controller: passwordTextController,
-                          obscureText: true,
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return "Password tidak boleh kosong";
-                            }
-                            return null;
-                          },
-                          decoration: InputDecoration(
-                            hintText: "Password",
-                            prefixIcon: const Icon(
-                              Icons.key,
-                              color: Colors.black,
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 20),
-                        ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.white,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                          ),
-                          onPressed: () {
-                            if (_formKey.currentState!.validate()) {
-                              print("Email: ${emailTextController.text}");
-                              print("Password: ${passwordTextController.text}");
-                            }
-                          },
-                          child: const Text(
-                            "Sign In",
-                            style: TextStyle(color: Colors.black),
-                          ),
-                        ),
-                        const SizedBox(height: 10),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
+                        )))),
+          ],
         ),
       ),
     );
